4b6da724281fef2b8bd4636e69f2ca3809b1666c
Devin Johnson <j00847797@students.jsums.edu>
2023-10-12 01:03:43 -0500

    completed db.go file and restructured oauth.go file

--- api/middleware/oauth.go
+++ api/middleware/oauth.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"log"
 	"os"
+	"encoding/base64"
+	"crypto/rand"
+	"fmt"
+	"io"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
@@ -12,79 +17,39 @@ import (
 	"golang.org/x/oauth2/google"
 )
 
-func init() {
+
+func LoadEnv() {
 	// Load .env file
-	if err := godotenv.Load(); err != nil {
+	if err := godotenv.Load("../../../.env"); err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 }
 
-var clientID = os.Getenv("GOOGLE_CLIENT_ID")
-var clientSecret = os.Getenv("GOOGLE_CLIENT_SECRET")
 
-var (
-	oauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:8000/api/survey", // Change this to your application's callback URL
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		Scopes:       []string{"openid", "profile", "email"},
-		Endpoint:     google.Endpoint,
-	}
-)
-
-func HandleGoogleAuth(c *fiber.Ctx) error {
-	// Generate URL and redirect to Google's OAuth page
-	url := oauthConfig.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	return c.Redirect(url)
+var oauthConfig = &oauth2.Config{
+	RedirectURL:  "http://localhost:8000/api/survey", // Change this to your application's callback URL
+	ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+	ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+	// Use the actual links for the string slice instead of only the keywords
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.name"},
+	Endpoint:     google.Endpoint,
 }
 
-func HandleGoogleCallback(c *fiber.Ctx) error {
-	// Get the code from the callback request
-	code := c.Query("code")
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
 
-	// Use the code to get the token from Google
-	token, err := oauthConfig.Exchange(context.Background(), code)
-	if err != nil {
-		return c.Status(400).SendString("Failed to exchange token: " + err.Error())
-	}
-
-	client := oauthConfig.Client(context.Background(), token)
-	resp, err := client.Get("https://www.googleapis.com/oauth2/v3/userinfo")
-	if err != nil {
-		// Handle error
-	}
-	defer resp.Body.Close()
-
-	// Decode the response to get user details
-	var userInfo map[string]interface{}
-	if err := json.NewDecoder(resp.Body).Decode(&userInfo); err != nil {
-		// Handle error
-	}
-
-	// Extract relevant data
-	email := userInfo["email"].(string)
-	name := userInfo["name"].(string)
-	password := userInfo["password"].(string)
-	hashPasword, err := HashPassword(password)
-	if err != nil {
-		// Handle error
-	}
-	// ... extract other details as needed
-
-	// Now, generate a JWT for the user
-	jwtToken, err := GenerateJWT(/* user data, if needed */)
-	if err != nil {
-		return c.Status(500).SendString("Failed to generate JWT: " + err.Error())
-	}
-
-	// Set the JWT in a cookie or return it in the response, based on your app's design
-	c.Set("Authorization", "Bearer "+jwtToken)
-	return c.Redirect("/api/survey")
+func oauthGoogleLogin(c *fiber.Ctx) error {
+	LoadEnv()
+	oauthState := generateStateOauthCookies(c)
+	u := oauthConfig.AuthCodeURL(oauthState)
+	return c.Redirect(u)
 }
 
-func IsAuthenticated(c *fiber.Ctx) error {
-	// Check if the user has an authenticated session or cookie
-	// If yes, proceed
-	// If not, redirect to login
-}
+
+
+
+
+
+
+
+
 
--- db/db.go
+++ db/db.go
@@ -1,2 +1,47 @@
 package db
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"github.com/joho/godotenv"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+	var err error
+
+	dsn := GetDBConfig()
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: &s", err.Error())
+	}
+}
+
+func GetDBConfig() string {
+
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    dbname := os.Getenv("DB_NAME")
+
+    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, port, dbname)
+    return dsn
+}
+
+func CloseDatabase() {
+    sqlDB, err := DB.DB()
+    if err != nil {
+        log.Fatalf("Failed to get underlying database from GORM: %s", err.Error())
+    }
+    sqlDB.Close()
+}
