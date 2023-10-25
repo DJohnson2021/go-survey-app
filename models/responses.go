c1534a6737338e136bffbe2625c3fe164cc5146b
DJohnson2021 <81720926+DJohnson2021@users.noreply.github.com>
2023-10-25 15:12:24 -0500

    Models (#2)
    * Added DeleteUser in users.go file and initial work done on questions.go file
    
    * Progress on database models
    
    * added ModifyUser function to users.go

--- models/responses.go
+++ models/responses.go
@@ -1,1 +1,48 @@
 package models
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/DJohnson2021/go-survey-app/db"
+	"gorm.io/gorm"
+)
+
+type Response struct {
+	ID            int32     `gorm:"primaryKey" gorm:"autoIncrement" db:"id" json:"id"`
+	Question_id   int32     `db:"question_id" json:"question_id"`
+	User_id       int32     `db:"user_id" json:"user_id"`
+	Response_text string    `db:"response_text" json:"response_text"`
+	Created_At    time.Time `db:"timestamp" json:"timestamp"`
+}
+
+func CreateResponse(response *Response) error {
+	// Create a new record in the database
+	if err := db.DB.Create(response).Error; err != nil {
+		return fmt.Errorf("error creating response: %v", err)
+	}
+	return nil
+}
+
+func DeleteResponseByID(id int32) error {
+	result := db.DB.Delete(&Response{}, "id = ?", id)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("Response record with id %v not found", id)
+		}
+		return result.Error
+	}
+	return nil
+}
+
+func ModifyResponse(response *Response) error {
+	result := db.DB.Save(response)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("Response record with id %v not found", response.ID)
+		}
+		return result.Error
+	}
+	return nil
+}
--- models/users.go
+++ models/users.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/DJohnson2021/go-survey-app/db"
@@ -9,20 +10,19 @@ import (
 )
 
 type User struct {
-	ID         int32     `db:"id" json:"id"`
+	ID         int32     `gorm:"primaryKey" gorm:"autoIncrement" db:"id" json:"id"`
 	GoogleID   string    `db:"google_id" json:"google_id"`
 	Username   string    `db:"username" json:"username"`
 	GivenName  string    `db:"given_name" json:"given_name"`
 	FamilyName string    `db:"family_name" json:"family_name"`
 	Email      string    `db:"email" json:"email"`
 	Created_At time.Time `db:"timestamp" json:"timestamp"`
-	// ... other fields
 }
 
 func CreateUser(user *User) error {
 	// Create a new record in the database
 	if err := db.DB.Create(user).Error; err != nil {
-		return err
+		return fmt.Errorf("error creating user: %v", err)
 	}
 	return nil
 }
@@ -32,9 +32,31 @@ func GetUserByID(google_id string) (*User, error) {
 	result := db.DB.First(&user, "google_id = ?", google_id)
 	if result.Error != nil {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			return nil, nil
+			return nil, fmt.Errorf("User record with google_id %v not found", google_id)
 		}
 		return nil, result.Error
 	}
 	return &user, nil
+}
+
+func DeleteUserByID(google_id string) error {
+	result := db.DB.Delete(&User{}, "google_id = ?", google_id)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("User record with google_id %v not found", google_id)
+		}
+		return result.Error
+	}
+	return nil
+}
+
+func ModifyUser(user *User) error {
+	result := db.DB.Save(user)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("User record with google_id %v not found", user.GoogleID)
+		}
+		return result.Error
+	}
+	return nil
 }
