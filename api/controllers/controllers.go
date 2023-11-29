6bb310d5b2671f5d12be07097243bf4d56c44061
Devin <j00847797@students.jsums.edu>
2023-11-29 02:41:18 -0600

    More work on the survey page

--- api/controllers/controllers.go
+++ api/controllers/controllers.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/DJohnson2021/go-survey-app/models"
 	// "log"
 	// "html/template"
 )
@@ -24,4 +25,28 @@ func SurveyPage(c *fiber.Ctx) error {
 // ResultPage is the controller for the survey results page.
 func ResultPage(c *fiber.Ctx) error {
 	return c.SendFile("../../templates/results.html")
+}
+
+func SubmitSurvey(c *fiber.Ctx) error {
+    // Assuming you have user authentication and can get the user's ID
+    userID := 
+
+    // Parse form values
+    question := "How much do you sleep?" // This can be dynamic based on your form
+    answer := c.FormValue("answer")
+
+    // Create a new survey response
+    response := models.SurveyResponse{
+        UserID:   userID,
+        Question: question,
+        Answer:   answer,
+    }
+
+    // Save the response to the database
+    result := db.DB.Create(&response)
+    if result.Error != nil {
+        return c.Status(fiber.StatusInternalServerError).SendString("Error saving response")
+    }
+
+    return c.Redirect("/survey-success")
 }
